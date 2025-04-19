a8c2d026b075e33113041cbfc266b5557b62a798
taposhsarker38 <taposhsarker38@gmail.com>
2025-04-19 14:14:34 +0600

    feat: addd properties,enquiries

---
+++ estate-src/apps/enquiries/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Enquiry
+# Register your models here.
+class EnquiryAdmin(admin.ModelAdmin):
+    list_display = ['name','phone_number','email','message']
+admin.site.register(Enquiry,EnquiryAdmin)
