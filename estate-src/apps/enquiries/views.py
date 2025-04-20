22a8ba8cc4a26c62d5939d61e41ee12049c198b4
taposhsarker38 <taposhsarker38@gmail.com>
2025-04-20 14:53:22 +0600

    feat: add ratings and enquiries

--- estate-src/apps/enquiries/views.py
+++ estate-src/apps/enquiries/views.py
@@ -1,6 +1,29 @@
-from rest_framework import serializers
+from django.core.mail import send_mail
+from real_estate.settings.development import DEFAULT_FROM_EMAIL
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from .models import Enquiry
-class EnquirySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Enquiry
-        fields = '__all__'
+@api_view(["POST"])
+@permission_classes([permissions.AllowAny])
+def send_enquiry_email(request):
+    data = request.data
+    try:
+        subject = data["subject"]
+        message = data["message"]
+        email = data["email"]
+        name = data["name"]
+        from_email = data["email"]
+        recipient_list = [DEFAULT_FROM_EMAIL]
+        send_mail(
+            subject,
+            message,
+            from_email,
+            recipient_list,
+            fail_silently=True,
+        )
+        enquiry = Enquiry(name=name, email=email, subject=subject, message=message)
+        enquiry.save()
+        return Response({"success": "Email sent successfully"})
+    except:
+        return Response({"fail": "Email sending failed"})
