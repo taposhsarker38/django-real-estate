388b2d7b835b395ca42daf57347d38bde7837af2
taposhsarker38 <taposhsarker38@gmail.com>
2025-06-01 09:49:26 +0600

    chore: setup basic home and properties page

--- estate-src/client/src/App.jsx
+++ estate-src/client/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import PropertiesPage from './pages/PropertiesPage';
+import { ToastContainer } from 'react-bootstrap';
 
 const App=()=> {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+          <Routes>
+            <Route path='/properties' element={<PropertiesPage />} />
+          </Routes>
+        </main>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
---
+++ estate-src/client/src/pages/PropertiesPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const PropertiesPage = () => {
+  return (
+    <>
+        <Container>
+            <Row>
+                <Col className='mg-top'>
+                    <h1>Properties</h1>
+                </Col>
+            </Row>
+        </Container>
+    </>
+  )
+}
+
+export default PropertiesPage
