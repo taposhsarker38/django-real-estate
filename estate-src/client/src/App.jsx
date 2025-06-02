76822732e24b28270d6cb0ffa8b2ee562c8954d9
taposhsarker38 <taposhsarker38@gmail.com>
2025-06-02 11:31:55 +0600

    feat: not found page

--- estate-src/client/src/App.jsx
+++ estate-src/client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import PropertiesPage from './pages/PropertiesPage';
 import { ToastContainer } from 'react-bootstrap';
+import NotFound from './components/NotFound';
 
 const App=()=> {
   return (
@@ -13,13 +15,12 @@ const App=()=> {
         <main className='py-3'>
           <Routes>
             <Route path='/' element={<HomePage />} />
-          </Routes>
-          <Routes>
             <Route path='/properties' element={<PropertiesPage />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
+      <ToastContainer  theme="dark"/>
         </main>
       </Router>
-      <ToastContainer />
     </>
   );
 }
